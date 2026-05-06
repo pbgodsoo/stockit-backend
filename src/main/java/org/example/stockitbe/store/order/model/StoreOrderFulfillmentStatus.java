16957d21bc65ff23345baef0bcdfa89e1454a796
saralove20 <saralove20@naver.com>
2026-05-07 03:10:28 +0900

    del: 발주 헤더에서 fulfillment_status 컬럼 제거, 발주 도메인에서 관련 코드를 제거

--- src/main/java/org/example/stockitbe/store/order/model/StoreOrderFulfillmentStatus.java
+++
@@ -1,9 +0,0 @@
-package org.example.stockitbe.store.order.model;
-
-public enum StoreOrderFulfillmentStatus {
-    READY_TO_SHIP,  // 배송 준비 중
-    IN_TRANSIT,     // 배송 중
-    ARRIVED,        // 매장 도착
-    RECEIVED        // 입고 완료
-}
-
