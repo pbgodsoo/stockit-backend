a449b024d71c5c0326161647ce638934b00a33b9
Beomsoo <linoleca@naver.com>
2026-05-06 10:18:34 +0900

    chore: import문 중복 제거

--- src/main/java/org/example/stockitbe/hq/inventory/InventoryRepository.java
+++ src/main/java/org/example/stockitbe/hq/inventory/InventoryRepository.java
@@ -5,15 +5,12 @@ import org.example.stockitbe.hq.inventory.model.Inventory;
 import org.example.stockitbe.hq.inventory.model.InventoryStatus;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-
-import jakarta.persistence.LockModeType;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
     List<Inventory> findAllBySkuIdIn(Collection<Long> skuIds);
