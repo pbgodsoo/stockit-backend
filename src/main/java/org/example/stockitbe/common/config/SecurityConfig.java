755aa066015afaec5d9901e19944539d2a9cb0b5
Beomsoo <linoleca@naver.com>
2026-05-15 00:03:15 +0900

    deploy: prometheus 추가

--- src/main/java/org/example/stockitbe/common/config/SecurityConfig.java
+++ src/main/java/org/example/stockitbe/common/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
         // 권한별 접근 제어
         http.authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/api/user/signup", "/api/user/login", "/api/user/logout", "/api/user/refresh", "/api/public/**").permitAll()        // 회원가입/로그인
-                .requestMatchers("/actuator/health", "/actuator/health/**", "/actuator/info").permitAll() // k8s probe/기본 상태 확인
+                .requestMatchers("/actuator/health", "/actuator/health/**", "/actuator/info", "/actuator/prometheus").permitAll() // k8s probe/메트릭 수집 허용
                 .requestMatchers("/api/hq/**").hasRole("HQ")                  // 본사
                 .requestMatchers("/api/store/**").hasRole("STORE")            // 매장
                 .requestMatchers("/api/warehouse/**").hasRole("WAREHOUSE")    // 창고
